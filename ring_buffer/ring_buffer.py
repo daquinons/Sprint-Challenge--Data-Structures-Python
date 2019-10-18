78869ca805fb5e4d97a58f49ac3031b0a31cd496
David Quiñones <daquinons@users.noreply.github.com>
2019-10-18 09:38:29 +0200

    Pass ring buffer test

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,11 +1,23 @@
+import collections
+
+
 class RingBuffer:
-  def __init__(self, capacity):
-    self.capacity = capacity
-    self.current = 0
-    self.storage = [None]*capacity
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.storage = collections.deque([None] * capacity, capacity)
+        self.times_written = 0
 
-  def append(self, item):
-    pass
+    @property
+    def current(self):
+        return len(self.storage)
 
-  def get(self):
-    pass
+    def append(self, item):
+        if None in self.storage:
+            self.storage.append(item)
+        else:
+            self.storage[self.times_written % self.capacity] = item
+
+        self.times_written += 1
+
+    def get(self):
+        return [element for element in self.storage if element is not None]
